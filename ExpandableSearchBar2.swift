a0482ad40d691f37ea026081cb4d9309034d6f01
Azharuddin <azharuddin.s@peopleinteractive.in>
2025-11-14 20:21:20 +0530

    bottom animated  bar - part 1

---
+++ ExpandableSearchBar2.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandableSearchBar.swift
+//  SwiftUIPlayground
+//
+//  Created by Azharuddin Salahuddin on 30/10/25.
+//
+
+import SwiftUI
+
+struct ExpandableSearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandableSearchBar()
+}
---
+++ SwiftUIPlayground/BottomBar.swift
@@ -0,0 +1,8 @@
+//
+//  BottomBar.swift
+//  SwiftUIPlayground
+//
+//  Created by Azharuddin Salahuddin on 11/11/25.
+//
+
+import Foundation
