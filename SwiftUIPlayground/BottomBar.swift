df639134e31374ea9cfd68d2ed1ef0fc5baab7b1
Azharuddin <azharuddin.s@peopleinteractive.in>
2025-11-14 20:22:28 +0530

    push changes

--- SwiftUIPlayground/BottomBar.swift
+++ SwiftUIPlayground/BottomBar.swift
@@ -6,3 +6,123 @@
 //
 
 import Foundation
+import SwiftUI
+struct AnimatedBottomBar<LeadingAction:View, TrailingAction:View, MainAction:View> : View {
+    
+    var hint: String
+    var tint: Color = .green
+    @Binding var text:String
+    @FocusState.Binding var isFocused: Bool
+    @ViewBuilder var leadingAction: () -> LeadingAction
+    @ViewBuilder var trailingAction: () -> TrailingAction
+    @ViewBuilder var mainAction: () -> MainAction
+    
+    
+    var body: some View  {
+        let mainLayout = isFocused ? AnyLayout(ZStackLayout(alignment: .bottomTrailing)) : AnyLayout(HStackLayout(alignment: .bottom, spacing: 10))
+        let shape = RoundedRectangle(cornerRadius: isFocused ? 25 : 30)
+        ZStack {
+            mainLayout {
+                let subLayout = isFocused ?
+                       AnyLayout(VStackLayout(alignment: .trailing, spacing: 20))
+                     : AnyLayout(ZStackLayout(alignment: .trailing))
+                subLayout {
+                    TextField(hint, text: $text, axis: .vertical)
+                        .lineLimit(5)
+                        .focused(_isFocused)
+                    
+                    HStack(spacing: 10) {
+                        ForEach(subviews: leadingAction()) { subView in
+                            subView
+                                .frame(width: 40, height: 40)
+                                .contentShape(.rect)
+                        }
+                    }
+                    .compositingGroup()
+                    
+                    Spacer(minLength: 0)
+                    
+                    trailingAction()
+                        .frame(width: 40, height: 40)
+                        .contentShape(.rect)
+                }
+                .frame(height: isFocused ? nil : 55)
+                .padding(.leading, 15)
+                .padding(.trailing, isFocused ? 15 : 10)
+                .padding(.bottom, isFocused ? 10 : 0)
+                .padding(.top, isFocused ? 20 : 0)
+                .background {
+                    shape
+                        .fill(.bar)
+                        .shadow(color: .black.opacity(0.1), radius: 5, x:0, y: 5)
+                        .shadow(color: .black.opacity(0.1), radius: 5, x:0, y: -5)
+                }
+            }
+        }
+        .geometryGroup()
+        .animation(.linear(duration: 1.0), value: isFocused)
+    }
+}
+
+
+#Preview {
+    Content()
+}
+
+
+private struct Content:View {
+    @State private var text = ""
+    @FocusState private var isFocused: Bool
+    var body: some View {
+        VStack {
+            Spacer(minLength: 0)
+            let fillColor = Color.gray.opacity(0.2)
+            AnimatedBottomBar(hint: "Type Here ...", text: $text, isFocused: $isFocused) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus")
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.primary)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(fillColor, in: .circle)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "magnifyingglass")
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.primary)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(fillColor, in: .circle)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "mic.fill")
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.primary)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(fillColor, in: .circle)
+                }
+
+            } trailingAction: {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "mic.fill")
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color.primary)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(fillColor, in: .circle)
+                }
+            } mainAction: {
+                
+            }
+        }
+        .padding(.horizontal, 15)
+        .padding(.bottom, 10)
+    }
+}
--- SwiftUIPlayground/ExpandableSearchBar2.swift
+++ SwiftUIPlayground/ExpandableSearchBar2.swift
@@ -9,10 +9,96 @@ import SwiftUI
 
 struct ExpandableSearchBar2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 15) {
+                ForEach(0..<100) { index in
+                    cardView()
+                }
+            }
+            .padding(15)
+            .safeAreaInset(edge: .top) {
+                ResizableHeader()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func ResizableHeader() -> some View {
+        VStack(spacing: 0) {
+            HStack {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Welcome Back!")
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                    
+                    Text("iJustine")
+                        .font(.title.bold())
+                }
+                
+                Spacer(minLength: 0)
+                
+                Button {
+                    
+                } label: {
+                    Image(.header)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 44, height: 44)
+                        .clipShape(Circle())
+                }
+            }
+            .padding(.horizontal,15)
+            .padding(.vertical,15)
+            
+            HStack(spacing: 8) {
+                Image(systemName: "magnifyingglass")
+                TextField("Search", text: .constant(""))
+            }
+            .padding(.horizontal,15)
+            .padding(.vertical,12)
+            .background {
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(
+                        .background
+                            .shadow(
+                            .drop(
+                                color: Color.black.opacity(0.08),
+                                radius: 5,
+                                x:5,
+                                y:5
+                            )
+                            
+                        )
+                            .shadow(
+                            .drop(
+                                color: Color.black.opacity(0.08),
+                                radius: 5,
+                                x:-5,
+                                y:-5
+                            )
+                            
+                        )
+                    )
+            }
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 15)
+    }
+    
+    @ViewBuilder
+    func cardView() -> some View {
+        VStack {
+            GeometryReader { geometry in
+                let size = geometry.size
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.cyan)
+                    .frame(width: size.width, height: size.height)
+            }
+            .frame(height: 220)
+        }
     }
 }
 
 #Preview {
-    ExpandableSearchBar()
+    ExpandableSearchBar2()
 }
