233a187d5c647d9907337035682f60b925a81a4f
Azharuddin <azharuddin.s@peopleinteractive.in>
2025-12-08 13:06:07 +0530

    pull rebase command

---
+++ SwiftUIPlayground/Git.swift
@@ -0,0 +1,22 @@
+
+//
+//  Git.swift
+//  SwiftUIPlayground
+//
+//  Created by Azharuddin Salahuddin on 07/12/25.
+//
+
+import Foundation
+func testGitMerger() {
+    func changeFrom() {
+         debugPrint("Change from main branch...")
+    }
+}
+
+
+
+func addNewMethod() {
+    debugPrint("Add new method...")
+}
+
+
