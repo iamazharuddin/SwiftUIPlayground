e83b551655c7b18bb27982facde06f49042545db
Azharuddin <azharuddin.s@peopleinteractive.in>
2025-12-21 22:03:04 +0530

    merge latest comit

---
+++ SwiftUIPlayground/architecture/ex.swift
@@ -0,0 +1,14 @@
+//
+//  ex.swift
+//  SwiftUIPlayground
+//
+//  Created by Azharuddin Salahuddin on 20/12/25.
+//
+
+/*
+ 
+ API -> DTO -> Mapper -> UseCase(Interactor) -> ViewModel -> View
+ 
+ ViewModel -> UseCase 
+ 
+ */
