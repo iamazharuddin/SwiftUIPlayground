bddcde14d2138ca8d007de441265951b7e88fc0a
Azharuddin <azharuddin.s@peopleinteractive.in>
2026-01-12 13:37:41 +0530

    Star Rating

--- SwiftUIPlayground/ComplexUI/MaskBootCamp.swift
+++ SwiftUIPlayground/ComplexUI/MaskBootCamp.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct MaskBootCamp: View {
+    @State private var rating:Int = 0
     var body: some View {
         HStack {
-            ForEach(0..<5) { _ in
+            ForEach(0..<5) { index in
                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundStyle(Color.gray)
+                    .frame(width: 50, height: 50)
+                    .onTapGesture {
+                        rating = index + 1
+                    }
+                    .allowsHitTesting(true)
             }
+        }
+        .overlay {
+            Rectangle()
+                .fill(Color.yellow)
+                .mask {
+                    HStack {
+                        ForEach(0..<5) { index in
+                           Image(systemName: "star.fill")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .opacity( index < rating ? 1 : 0)
+                        }
+                    }
+                }
+                .allowsHitTesting(false)
         }
     }
 }
