f65c9af3da904f126f909422540216726a75f591
Azharuddin <azharuddin.s@peopleinteractive.in>
2026-01-30 15:11:01 +0530

    Combine Networking  - MyEndpoint setup

--- SwiftUIPlayground/Networking/Networking.swift
+++ SwiftUIPlayground/Networking/Networking.swift
@@ -5,3 +5,11 @@
 //  Created by Azharuddin Salahuddin on 30/01/26.
 //
 
+import Foundation
+protocol MyEndpoint {
+         var baseURL:String { get }
+         var path: String { get }
+         var method: String { get }
+         var parameter:[URLQueryItem] { get }
+         var headers:[String:String] { get }
+}
